b46b28dc7af80143990b03d02d8cd3573b92d1e5
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-06-16 08:44:41 +0900

    Get many parameters correctly #5

--- src/index.js
+++ src/index.js
@@ -28,9 +28,7 @@ export function ssmToObj(options, callback) {
       return callback(null, target);
     }
 
-    const ssm = getSsm();
-
-    ssm.getParameters({ Names, WithDecryption: withDecryption }, (err, data) => {
+    getParameters({ Names, WithDecryption: withDecryption }, (err, data) => {
       if (err) {
         return callback(err);
       }
@@ -52,6 +50,58 @@ export function ssmToObj(options, callback) {
       callback(null, target);
     });
   });
+}
+
+function getParameters(params, callback) {
+  const ssm = getSsm();
+
+  const { Names } = params;
+
+  if (Names.length <= 10) {
+    return ssm.getParameters(params, callback);
+  }
+
+  /*
+   * Call getParameters in parallel for many names.
+   */
+  const { WithDecryption } = params;
+  const mergedData = {
+    Parameters: [],
+    InvalidParameters: [],
+  };
+  let count = (Names.length + 9) / 10 | 0;
+
+  for (let i = 0; i < Names.length; i += 10) {
+    ssm.getParameters({ Names: Names.slice(i, i + 10), WithDecryption }, (err, data) => {
+      /*
+       * If callback is called already, discard the data.
+       */
+      if (count <= 0) {
+        return;
+      }
+
+      /*
+       * Call the callback once only for an error.
+       */
+      if (err) {
+        count = 0;
+        return callback(err, null);
+      }
+
+      /*
+       * Merge data.
+       */
+      mergedData.Parameters.push(...(data.Parameters || []));
+      mergedData.InvalidParameters.push(...(data.InvalidParameters || []));
+
+      /*
+       * If all getParameters call is completed, call the callback with merged data.
+       */
+      if (--count === 0) {
+        return callback(null, mergedData);
+      }
+    });
+  }
 }
 
 export function ssmToEnv(prefix, callback) {
