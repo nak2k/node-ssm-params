970c5e194dc57009cda7b16392db9405b3a8e662
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-06-27 09:07:01 +0900

    Set default region for testing

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const {
 } = require('..');
 const AWS = require('aws-sdk');
 
-AWS.config.update({ region: process.env.AWS_REGION });
+AWS.config.update({ region: process.env.AWS_REGION || 'us-west-2' });
 
 test('test ssmToObj', t => {
   t.plan(2);
